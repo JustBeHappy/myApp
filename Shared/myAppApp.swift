444131201aad7c55445b2c170103baafe6217774
郭成盖 <guochenggai@192.168.2.72>
2021-12-29 22:08:09 +0800

    init

---
+++ Shared/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  Shared
+//
+//  Created by 郭成盖 on 2021/12/29.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
